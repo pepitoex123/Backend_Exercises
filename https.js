be198906ad41e077fe28ea58a06f03f5e0afe981
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-02 21:54:40 -0300

    Testing modules

---
+++ https.js
@@ -0,0 +1,14 @@
+// module test
+
+const request = require("./request");
+const response = require("./response")
+
+
+function makeRequest(url,data){
+    request.send(url,data);
+    return response.read();
+}
+
+const responseData = makeRequest("https://www.google.com","data");
+
+console.log(responseData)
