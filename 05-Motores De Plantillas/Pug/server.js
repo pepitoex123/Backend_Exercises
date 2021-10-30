85f237773ee655de17ca720c7f32aeb47568c9ab
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-30 16:34:15 -0300

    added pug example

--- 05-Motores De Plantillas/Pug/server.js
+++ 05-Motores De Plantillas/Pug/server.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const Contenedor = require("./contenedor");
+const pug = require("pug");
 
 
 
 const productsRouter = require("./routes/productos");
 
+const viewsRouter = require("./routes/publicRendering");
+
 
 const app = express();
 
 
+app.set("view engine","pug");
+
+app.set("views","./views");
+
+
+app.use(express.static("public"));
+
+
 app.use(express.json());
+
+app.use(express.urlencoded());
 
 
 app.use("/api/productos",productsRouter)
+
+app.use("/",viewsRouter)
 
 
 app.listen(3000);
